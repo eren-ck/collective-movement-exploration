0c65895efc64030ccbd379ddf279f830a5317187
CWebkas <lukeweixler@googlemail.com>
2020-06-09 20:07:06 +0200

    cleaned code

--- src/app/server/feature_extraction/calculate_features.py
+++ src/app/server/feature_extraction/calculate_features.py
@@ -35,9 +35,12 @@ def calculate_features(id, movement_file_filename, metadata_file_filename, image
     # # calculate swarm features
     calculate_swarm_features(id)
     # # calculate percentiles
+    print('STARTING PERCENTILES')
     calculate_percentiles(id)
     # # calculate several basic networks for each dataset automatically with the upload
-    # calculate_basic_networks(id)
+
+    print('STARTING NETWORK')
+    calculate_basic_networks(id)
 
 
 def upload_data(id, movement_file_filename, metadata_file_filename, image_name):
--- src/app/server/feature_extraction/swarm_features.py
+++ src/app/server/feature_extraction/swarm_features.py
@@ -4,7 +4,7 @@ from model.movement_data_model import Movement_data
 from db import create_session
 from functools import reduce
 
-from geoalchemy2 import functions
+from geoalchemy2 import functions, elements
 import pandas as pd
 import numpy as np
 import movekit as mkit
@@ -29,18 +29,25 @@ def calculate_swarm_features(id):
     # for every animal get the movement data
     # and extrat the absolute features
     try:
-                # calculate the centroid
-        calculate_centroid(id, session)
-        # calculate the convex hull area
-        calculate_convex_hull(id, session)
-        # calculate convex hull area
-        calculate_convex_hull_area(id, session)
-        # calculate delaunay triangulation
-        calculate_delaunay_triangulation(id, session)
-        # calculate voronoi diagram
-        calculate_voronoi_diagram(id, session)
 
         calculate_mkit_feats(id, session)
+        print('MKIT DONE')
+                # calculate the centroid
+        #calculate_centroid(id, session)
+        # calculate the convex hull area
+        calculate_convex_hull(id, session)
+        print('CONV HULL DONE')
+
+        # calculate convex hull area
+        calculate_convex_hull_area(id, session)
+        print('CONV AREA DONE')
+        # calculate delaunay triangulation
+        calculate_delaunay_triangulation(id, session)
+        print('DELAUNAY DONE')
+        # calculate voronoi diagram
+        calculate_voronoi_diagram(id, session)
+        print('VORONOI DONE')
+
         # complete
         dataset[0].status = 'Complete'
         dataset[0].progress = 100
@@ -56,24 +63,6 @@ def calculate_swarm_features(id):
 
     # remove the session
     session.remove()
-
-
-def calculate_centroid(id, session):
-    """ Calculate the centroid of the animal group
-    Keyword arguments:
-    id - id of the dataset
-    session - db session
-    """
-    query = '''UPDATE   group_data
-                SET 	centroid =  subquery.geom
-                FROM ( SELECT "time", ST_Centroid(ST_Collect(position)) AS geom
-                                        FROM movement_data
-                                        WHERE dataset_id = :id
-                                        GROUP BY "time") AS subquery
-                WHERE   group_data.time = subquery.time
-                        AND group_data.dataset_id = :id;'''
-    session.execute(query, {'id': id})
-
 
 def calculate_convex_hull(id, session):
     """ Calculate the convex hull of the animal group
@@ -152,6 +141,8 @@ def calculate_mkit_feats(id, session):
     id - id of the dataset
     session - db session
     """
+
+    # Get features from database
     query = session.query(Movement_data.time,
                           Movement_data.animal_id,
                           Movement_data.direction,
@@ -160,64 +151,47 @@ def calculate_mkit_feats(id, session):
         .filter_by(dataset_id=id).order_by('time')
     # read into pandas frame for faster calculation
     df = pd.read_sql(query.statement, query.session.bind)
+
+    # prepare for analysis
     df.rename(columns={'ST_X_1': 'x', 'ST_Y_1': 'y'}, inplace=True)
 
-
-    # calculate the distance to centroid store in movement data
     # calculate the centroids and medoids, store in group data
-    movement = mkit.centroid_medoid_computation(df)
+    movement = mkit.centroid_medoid_computation(df, object_output = True)
 
-    group = movement.loc[movement.animal_id == list(set(movement.animal_id))[0], ['time', 'x_centroid', 'y_centroid', 'medoid']].reset_index(drop=True)
+    # prepare for merge
+    movement = movement.rename(columns = {'distance_to_centroid':'distance_centroid'})
 
+    # merge movement data with distance_centroid
+    for index, row in movement.iterrows():
+        query = Movement_data(dataset_id=id, **OrderedDict(row))
+        session.merge(query)
 
-    # compute polarization, store in group data
+    session.commit()
+    print('DIST CEN DONE')
+    # Take subset from dataset above, focusing only on group-level
+    group = movement.loc[movement.animal_id == list(set(movement.animal_id))[0], ['time', 'x_centroid', 'y_centroid', 'medoid', 'centroid']].reset_index(drop=True)
+
+    # compute polarization
     pol = mkit.compute_polarization(df, group_output = True).fillna(0)
 
-    # compute mean speed, acceleration and mean distance to centroid, store in group data
+    # compute mean speed, acceleration and mean distance to centroid
     mov = mkit.group_movement(movement).fillna(0)
 
-    # compute centroid direction, store in group_data
+    # compute centroid direction
     cen_dir = mkit.compute_centroid_direction(movement, group_output = True).fillna(0)
 
 
-
+    # merge computed values into group-dataframe
     data_frames = [group, pol, mov, cen_dir]
     group = reduce(lambda  left,right: pd.merge(left,right,on=['time'],
                                                 how='outer'), data_frames)
-
-
-    movement = movement.drop(columns = ["x_centroid", "y_centroid", "medoid", "x", "y"])
-    movement = movement.rename(columns = {'distance_to_centroid':'distance_centroid'})
-    for index, row in movement.iterrows():
-        query = Group_data(dataset_id=id, **OrderedDict(row))
-        session.merge(query)
-
-    session.commit()
-
-
-
-    group = group.drop(columns = ["x_centroid", "y_centroid"])
+    # prepare for merging with database
     group = group.rename(columns = {'mean_distance_centroid':'distance_centroid','centroid_direction':'direction','mean_speed':'speed','mean_acceleration':'acceleration','mean_distance_centroid':'distance_centroid', 'total_dist': 'metric_distance', 'polarization': 'polarisation'})
 
-    query = session.query(Group_data.time, Group_data.polarisation).filter_by(dataset_id=id).order_by('time')
-    # read into pandas frame for faster calculation
-    df = pd.read_sql(query.statement, query.session.bind)
-
-    df = group
-
-    # Extracting the features via mkit
-
+    # first convert centroid coordinates to shape and delete them, then run query.
     for index, row in group.iterrows():
-        query = Group_data(dataset_id=id, **OrderedDict(row))
+        query = Group_data(dataset_id=id,**OrderedDict(row))
         session.merge(query)
-
-    # change the progress bar
+    print('GROUP FEATS DONE')
     # add the data to the database
     session.commit()
-
-
-    #group_dat = Group_data.query.get(2)
-
-    #print(group_dat)
-    # commit
-    #session.commit()
