aa2950663ce82d4b7f8962bdc03cfb9ed327d91d
CWebkas <lukeweixler@googlemail.com>
2020-08-01 16:04:05 +0200

    linechart and tank run synchrnonycally

--- src/app/front-end/explore/ajax_queries.js
+++ src/app/front-end/explore/ajax_queries.js
@@ -39,6 +39,7 @@ import {
  * Stream the movement data from the API
  * Loads only the explicit movement data
  */
+//console.log(data);
 export function streamMovementData() {
     if (window.EventSource) {
         source = new EventSource('/api/movement_only/' + parameters['id']);
@@ -51,6 +52,7 @@ export function streamMovementData() {
                         if ($.active > 0) {
                             window.setTimeout(checkPendingRequest, 100);
                         } else {
+
                             var view = new SpatialView();
                         }
                     }
